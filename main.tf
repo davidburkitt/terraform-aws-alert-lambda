c86496d60db34f36b5113fa11ec8f51e99fb0cd9
David Burkitt <davidburkitt@hotmail.com>
2020-05-21 10:34:17 +0100

    removed filter and metric modules (not required)

--- main.tf
+++ main.tf
@@ -9,22 +9,31 @@ provider "aws" {
   shared_credentials_file = var.aws_creds
 }
 
-module "log_filter" {
-  source = "../terraform-aws-log-filter"
-  #source = "github.com/davidburkitt/terraform-aws-log-filter"
-  aws_creds = var.aws_creds
-  aws_region = var.aws_region
-  #count = length(var.log_filters)
-  log_filter = var.log_filter
+resource "aws_cloudwatch_log_metric_filter" "log_metric_filter" {
+  name           = var.log_filter.name
+  pattern        = var.log_filter.pattern
+  log_group_name = var.log_filter.log_group
+
+  metric_transformation {
+    name      = var.log_filter.metric_name
+    namespace = var.log_filter.metric_name_space
+    value     = var.log_filter.metric_value
+    default_value = var.log_filter.metric_default_value
+  }
 }
 
-module "metric_alarm" {
-  source = "../terraform-aws-metric-alarm"
-  #source = "github.com/davidburkitt/terraform-aws-metric-alarm"
-  aws_creds = var.aws_creds
-  aws_region = var.aws_region
-  #count = length(var.metric_alarms)
-  metric_alarm = var.metric_alarm
+resource "aws_cloudwatch_metric_alarm" "metric_alarm" {
+  alarm_name                = var.metric_alarm.name
+  alarm_description         = var.metric_alarm.description
+  metric_name               = var.metric_alarm.metric_name
+  namespace                 = var.metric_alarm.metric_namespace
+  statistic                 = var.metric_alarm.statistic
+  comparison_operator       = var.metric_alarm.comparitor
+  period                    = var.metric_alarm.period
+  evaluation_periods        = var.metric_alarm.periods
+  threshold                 = var.metric_alarm.threshold
+  insufficient_data_actions = var.metric_alarm.insufficient_data_actions
+  alarm_actions             = var.metric_alarm.alarm_actions
 }
 
 module "lambda_function" {
--- output.tf
+++ output.tf
@@ -1,4 +1,5 @@
 
+/*
 output "log_filter" {
   value = module.log_filter.log_filters_output
   description = "log filter name"
@@ -6,6 +7,17 @@ output "log_filter" {
 
 output "metric_alarm" {
   value = module.metric_alarm.metric_alarms_output
+  description = "metric alarm name"
+}
+*/
+
+output "log_filter" {
+  value = aws_cloudwatch_log_metric_filter.log_metric_filter.name
+  description = "log filter name"
+}
+
+output "metric_alarm" {
+  value = aws_cloudwatch_metric_alarm.metric_alarm.alarm_name
   description = "metric alarm name"
 }
 
